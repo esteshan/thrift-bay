c355cb9375d1292420c76cd0596c9926b5f0c1fa
Joshua Ola <joshuaola23@gmail.com>
2023-10-11 00:31:24 +0000

    Resolve "Adding auth Headers to all the backend endpoints"

--- api/routers/categories.py
+++ api/routers/categories.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, Depends
+from authenticator import authenticator
 from typing import Union, List
 from queries.categories import (
     CategoryIn,
@@ -14,6 +15,7 @@ router = APIRouter()
 @router.post("/categories", response_model=CategoryOut)
 def create_category(
     category: CategoryIn,
+    account_data: dict = Depends(authenticator.get_current_account_data),
     repo: CategoryRepository = Depends(),
 ):
     return repo.create_category(category)
