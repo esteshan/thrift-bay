38c4d69cf2b61965a939b3e59f060a4fcb4d1ca5
Joshua Ola <joshuaola23@gmail.com>
2023-10-13 21:29:13 +0000

    Resolve "Implement Detailed View for Individual Products"

--- ghi/src/components/Navbar.js
+++ ghi/src/components/Navbar.js
@@ -6,12 +6,15 @@ function Nav() {
             <div className="container-fluid">
                 <NavLink className="navbar-brand" to="/">thriftBay</NavLink>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
+                    <span className="navbar-toggler-icon"></span>
                 </button>
                 <div>
                     <ul>
                         <li className="nav-item">
                             <NavLink className="nav-link" to="/...enter pg name">Display Page Name</NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink className="nav-link" to="/products">Product</NavLink>
                         </li>
                     </ul>
                 </div>
