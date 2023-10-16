9af410ec7a37758382297c25ecb35bdd14e5a1b5
esteshan100 <esteshan100@gmail.com>
2023-10-16 11:21:12 -0700

    added main page

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -1,12 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Nav from './components/Navbar';
 import ProductDetail from "./pages/ProductDetail";
-
-
-
-
-
-
+import Home from "./pages/Home/Home";
 
 function App() {
   return (
@@ -14,6 +9,7 @@ function App() {
     <Nav />
     <div className="container"></div>
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="products/:product_id" element={<ProductDetail />} />
       </Routes>
     </BrowserRouter>
