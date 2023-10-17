392a7528b9d2c53c504fc00e36215dfe02c9ffed
esteshan100 <esteshan100@gmail.com>
2023-10-17 16:41:18 -0700

    updated tailwind

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -5,6 +5,7 @@ import LoginForm from './pages/Login/LoginForm';
 import Nav from './components/Navbar';
 import ProductDetail from "./pages/ProductDetail";
 import Home from "./components/Home";
+import Test from "./components/Test";
 
 function App() {
   const domain = /https:\/\/[^/]+/;
@@ -16,6 +17,7 @@ function App() {
         <AuthProvider baseUrl={process.env.REACT_APP_API_HOST}>
           <Routes>
             <Route path="/" element={<Home />}></Route>
+            <Route path="/test" element={<Test />}></Route>
             <Route path="/signup" element={<SignupForm />}></Route>
             <Route path="/login" element={<LoginForm />}></Route>
             <Route path="products/:product_id" element={<ProductDetail />} />
