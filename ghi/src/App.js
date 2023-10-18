210c4efd1d28765eec6c21d19c7d60b7b3394aaa
Joshua Ola <joshuaola23@gmail.com>
2023-10-18 19:46:31 +0000

    Resolve "Implement redux/rtk on product detail page"

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -3,10 +3,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import SignupForm from './pages/Signup/SignupForm';
 import LoginForm from './pages/Login/LoginForm';
 import Nav from './components/Navbar';
-import ProductDetail from "./pages/ProductDetail";
 import Home from "./components/Home";
-import Home from "./components/Home";
-import Home from "./components/Home";
+import ProductDetail from './components/ProductDetail';
 
 function App() {
   const domain = /https:\/\/[^/]+/;
@@ -20,7 +18,7 @@ function App() {
             <Route path="/" element={<Home />}></Route>
             <Route path="/signup" element={<SignupForm />}></Route>
             <Route path="/login" element={<LoginForm />}></Route>
-            <Route path="products/:product_id" element={<ProductDetail />} />
+            <Route path="/products/:product_id" element={<ProductDetail />}></Route>
           </Routes>
         </AuthProvider>
       </BrowserRouter>
--- ghi/src/store/store.js
+++ ghi/src/store/store.js
@@ -1,13 +1,18 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { setupListeners } from '@reduxjs/toolkit/query';
 import { productsApi } from "./productsApi";
+import { productDetailApi } from './detailApi';
 
 export const store = configureStore({
 reducer: {
     [productsApi.reducerPath]: productsApi.reducer,
-   },
-   middleware: getDefaultMiddleware=>
-   getDefaultMiddleware().concat(productsApi.middleware),
+    [productDetailApi.reducerPath]: productDetailApi.reducer,
+    },
+    middleware: getDefaultMiddleware=>
+    getDefaultMiddleware().concat(
+    productsApi.middleware,
+    productDetailApi.middleware
+    ),
 });
 
 setupListeners(store.dispatch);
