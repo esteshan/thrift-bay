3974b40b521169ce797d98f759cefde6cd46663e
esteshan100 <esteshan100@gmail.com>
2023-10-18 12:38:45 -0700

    working on bav

--- ghi/src/components/Navbar.js
+++ ghi/src/components/Navbar.js
@@ -1,27 +1,36 @@
-import { NavLink } from 'react-router-dom';
+import React from "react";
+import { NavLink } from "react-router-dom";
 
-function Nav() {
-    return(
-        <nav className="navbar navbar-expand-lg navbar-dark bg-success">
-            <div className="container-fluid">
-                <NavLink className="navbar-brand" to="/">thriftBay</NavLink>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div>
-                    <ul>
-                        <li className="nav-item">
-                            <NavLink className="nav-link" to="/...enter pg name">Display Page Name</NavLink>
-                        </li>
-                        <li className="nav-item">
-                            <NavLink className="nav-link" to="/products">Product</NavLink>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-    )
-
-}
+const Nav = () => {
+  return (
+    <div className="items-center border-b-2 justify-between py-3 px-8 flex min-h-[4.06rem] border-zinc-300 border-solid">
+      {/* ... your code ... */}
+      <section className="bottom-[13.75rem] left-[2.00rem] absolute right-[189.75rem] top-[-6.25rem]">
+        {/* ... skipped for brevity ... */}
+      </section>
+      <a
+        href="https://www.depop.com/"
+        className="text-blue-700 cursor-pointer underline"
+      >
+        {/* ... skipped SVG for brevity ... */}
+      </a>
+      <div>
+        <div className="flex-col flex ml-6">
+          <form
+            style={{ maxWidth: "44.00rem" }}
+            className="relative flex w-[calc(42vw)] m-auto"
+          >
+            {/* ... skipped for brevity ... */}
+          </form>
+        </div>
+      </div>
+      <nav className="flex h-8">
+        <ul className="items-center justify-between flex list-disc -ml-1 gap-1">
+          {/* ... skipped list items for brevity ... */}
+        </ul>
+      </nav>
+    </div>
+  );
+};
 
 export default Nav;
