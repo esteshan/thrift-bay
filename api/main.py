c8d0e0bfc55284e8bf1970d0ba90a775887b667a
Francisco Real <esteshan100@gmail.com>
2023-10-24 19:59:50 +0000

    Resolve "Creating auth slice"

--- api/main.py
+++ api/main.py
@@ -13,14 +13,14 @@ def root():
     return {"message": "You hit the root path!"}
 
 
+origins = [
+    "http://localhost:3000",
+    os.environ.get("CORS_HOST", None),
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        os.environ.get(
-            "CORS_HOST",
-            "http://localhost:3000",
-        ),
-    ],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
--- ghi/src/store/store.js
+++ ghi/src/store/store.js
@@ -1,24 +1,28 @@
-import { configureStore } from '@reduxjs/toolkit';
-import { setupListeners } from '@reduxjs/toolkit/query';
+import { configureStore } from "@reduxjs/toolkit";
+import { setupListeners } from "@reduxjs/toolkit/query";
 import { productsApi } from "./productsApi";
-import { productDetailApi } from './detailApi';
+import { productDetailApi } from "./detailApi";
 import { usersApi } from "./userProfileApi";
-import { categoryApi } from './categoryApi';
+import { categoryApi } from "./categoryApi";
+import { authApi } from "./authApi";
 
 export const store = configureStore({
-reducer: {
+  reducer: {
+    [authApi.reducerPath]: authApi.reducer,
     [productsApi.reducerPath]: productsApi.reducer,
     [productDetailApi.reducerPath]: productDetailApi.reducer,
     [usersApi.reducerPath]: usersApi.reducer,
     [categoryApi.reducerPath]: categoryApi.reducer,
-    },
-    middleware: (getDefaultMiddleware) =>
+  },
+  middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(
-    productsApi.middleware,
-    productDetailApi.middleware,
-    usersApi.middleware,
-    categoryApi.middleware,
-  ),
+      authApi.middleware,
+      productsApi.middleware,
+      productDetailApi.middleware,
+      usersApi.middleware,
+      categoryApi.middleware,
+      usersApi.middleware,
+    ),
 });
 
 setupListeners(store.dispatch);
