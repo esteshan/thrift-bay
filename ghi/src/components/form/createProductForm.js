42fadd5f3156af34cd5af500855c9e4d8f9c49c3
wyatt Sanchez <wyattsanchez007@gmail.com>
2023-10-25 18:31:34 -0700

    finished create product page

--- ghi/src/components/form/createProductForm.js
+++ ghi/src/components/form/createProductForm.js
@@ -1,26 +1,42 @@
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import React from "react";
 import { useCreateProductMutation } from "../../store/newProductApi";
-import { useGetCategoryQuery } from "../../store/categoryApi";
 import { useGetTokenQuery } from "../../store/authApi";
-
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useGetCategoryQuery } from "../../store/categoryApi";
 
 function NewProduct() {
-    const { data: categories, isLoading, isError } = useGetCategoryQuery();
-    const { data: user } = useGetTokenQuery();
-    const [ name, setName ] = useState("")
-    const [ picture_url, setPicture_url ] = useState("")
-    const [ color, setColor ] = useState("")
-    const [ size, setSize ] = useState("")
-    const [ description, setDescription ] = useState("")
-    const [ item_price, setItem_price ] = useState(0)
-    const [ error, setError ] = useState("")
-    const [ created_at, setCreated_at ] = useState("")
-    const [ sold ] = useState(false)
-    const [ category, setCategory ] = useState("")
-    const [createProduct, result] = useCreateProductMutation();
-    const user_id  = user?.user.user_id;
     const navigate = useNavigate();
+    const { data: user } = useGetTokenQuery();
+    const { data: categories, isLoading, isError } = useGetCategoryQuery();
+    const [createProduct, result] = useCreateProductMutation();
+    const [name, setName] = useState("");
+    const [picture_url, setPictureURL] = useState("");
+    const [color, setColor] = useState("");
+    const [size, setSize] = useState("");
+    const [description, setDescription] = useState("");
+    const [item_price, setItemPrice] = useState(0);
+    const [category, setCategory] = useState("");
+    const [user_id, setUserID] = useState(user ? user.user.user_id : "");
+    const [error, setError] = useState("");
+    const [created_at, setCreated_at] = useState("");
+    const [sold, setSold] = useState(false);
+
+    useEffect(() => {
+        if (user && user.user) {
+        setUserID(user.user.user_id);
+        }
+    }, [user]);
+
+  // Add this useEffect to handle redirection after successful product creation
+    useEffect(() => {
+        if (result.isSuccess) {
+        navigate("/");
+        }
+        if (result.isError) {
+        setError("An error occurred while creating the product");
+        }
+    }, [result, navigate]);
 
 
     if (isLoading) {
@@ -32,168 +48,115 @@ function NewProduct() {
     }
 
 
-    // function handleCreate() {
-    //     if (products){
-    //         navigate("/")
-    //     }
-    // }
-
-
-    const handleSubmit = async (e) => {
-    e.preventDefault();
-    if (user_id){
-        const createProductData = {
-            name,
-            picture_url,
-            color,
-            size,
-            description,
-            item_price,
-            sold,
-            category,
-            user_id,
-            created_at
-        };
-        try {
-            await createProduct(createProductData).unwrap();
-            navigate("/")
-        } catch (error) {
-            console.log("Create product failed:", error);
-        }
+    async function handleSubmit(e) {
+        e.preventDefault();
+        createProduct({
+        name,
+        picture_url,
+        color,
+        size,
+        description,
+        item_price,
+        sold,
+        category,
+        user_id,
+        created_at,
+        });
     }
-
-    // function handleCreate() {
-    //     if (products){
-    //         navigate("/")
-    //     }
-    // }
-
-
-    // Basic validation
-        if (!name) {
-            setError("Name cannot be empty");
-            return;
-        }
-        if (!picture_url) {
-            setError("Picture url cannot be empty");
-            return;
-        }
-        if (!color) {
-            setError("Color cannot be empty");
-            return;
-        }
-        if (!size) {
-            setError("Size cannot be empty");
-            return;
-        }
-        if (!description) {
-            setError("description cannot be empty");
-            return;
-        }
-        if (!item_price) {
-            setError("item cannot be empty");
-            return;
-        }
-        if (!categories) {
-            setError("please select a category");
-            return;
-        }
-
-    }
-
 
     return (
-        <>
-            <h1>Sell a Product</h1>
-                <form onSubmit={handleSubmit}>
-                    <div>
-                        <input
-                            type="text"
-                            id="name"
-                            placeholder="Product name"
-                            value={name}
-                            onChange={(e) => setName(e.target.value)}
-                        />
-                    </div>
-                    <div>
-                        <input
-                            type="text"
-                            id="picture_url"
-                            placeholder="Picture Url"
-                            value={picture_url}
-                            onChange={(e) => setPicture_url(e.target.value)}
-                        />
-                    </div>
-                    <div>
-                        <input
-                            type="text"
-                            id="color"
-                            placeholder="Product Color"
-                            value={color}
-                            onChange={(e) => setColor(e.target.value)}
-                        />
-                    </div>
-                    <div>
-                        <input
-                            type="text"
-                            id="size"
-                            placeholder="Product Size"
-                            value={size}
-                            onChange={(e) => setSize(e.target.value)}
-                        />
-                    </div>
-                    <div>
-                        <textarea
-                            type="text"
-                            id="description"
-                            placeholder="Product description"
-                            value={description}
-                            onChange={(e) => setDescription(e.target.value)}
-                        />
-                    </div>
-                    <div>
-                        <input
-                            type="number"
-                            id="item_price"
-                            placeholder="Product Price"
-                            value={item_price}
-                            onChange={(e) => setItem_price(e.target.value)}
-                        />
-                    </div>
-                    <div>
-                        <label htmlFor="created_at">Created At:</label>
-                        <input
-                            type="date"
-                            id="created_at"
-                            value={created_at}
-                            onChange={(e) => setCreated_at(e.target.value)}
-                        />
-                    </div>
-                    <div>
-                        <select
-                            onChange={(e) => setCategory(e.target.value)}
-                            required
-                            id="category"
-                            className="form-select"
-                            >
-                            <option value="">Choose a category</option>
-                            {categories.map((category) => {
-                                return (
-                                <option key={category.category_id} value={category.category_id}>
-                                    {category.name}
-                                </option>
-                                );
-                            })}
-                        </select>
-                    </div>
-                    <button type="submit">Create Product</button>
+        <form onSubmit={handleSubmit}>
+        <div>
+            <label htmlFor="name">Name:</label>
+            <input
+            type="text"
+            id="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            />
+        </div>
+        <div>
+            <label htmlFor="picture_url">Picture URL:</label>
+            <input
+            type="text"
+            id="picture_url"
+            value={picture_url}
+            onChange={(e) => setPictureURL(e.target.value)}
+            />
+        </div>
+        <div>
+            <label htmlFor="color">Color:</label>
+            <input
+            type="text"
+            id="color"
+            value={color}
+            onChange={(e) => setColor(e.target.value)}
+            />
+        </div>
+        <div>
+            <label htmlFor="size">Size:</label>
+            <input
+            type="text"
+            id="size"
+            value={size}
+            onChange={(e) => setSize(e.target.value)}
+            />
+        </div>
+        <div>
+            <label htmlFor="description">Description:</label>
+            <input
+            type="text"
+            id="description"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+            />
+        </div>
+        <div>
+            <label htmlFor="item_price">Item Price:</label>
+            <input
+            type="number"
+            id="item_price"
+            value={item_price}
+            onChange={(e) => setItemPrice(Number(e.target.value))}
+            />
+        </div>
+        <div className="mb-3">
+            <select
+            onChange={(e) => setCategory(e.target.value)}
+            required
+            id="category"
+            className="form-select"
+            >
+            <option value="">Choose a category</option>
+            {categories.map((category) => {
+                return (
+                <option key={category.category_id} value={category.category_id}>
+                    {category.name}
+                </option>
+                );
+            })}
+            </select>
+        </div>
+        <div>
+            <label htmlFor="user_id">User ID:</label>
+            <input id="user_id" value={user.user.username} readOnly={true} />
+        </div>
+        <div>
+            <label htmlFor="created_at">Created At:</label>
+            <input
+            type="date"
+            id="created_at"
+            value={created_at}
+            onChange={(e) => setCreated_at(e.target.value)}
+            />
+        </div>
+        <button type="submit" disabled={result.isLoading}>
+            Create Product
+        </button>
 
-                        {result.isLoading && <p>Loading...</p>}
-                        {error && <p>Error: {error}</p>}
-                </form>
-        </>
+        {result.isLoading && <p>Loading...</p>}
+        {error && <p>Error: {error}</p>}
+        </form>
     );
 }
-
-
-export default NewProduct
+export default NewProduct;
