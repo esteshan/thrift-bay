1270418cf3e319ce9044aae9a3b64027aa6a64b2
Josh <joshuaola23@gmail.com>
2023-10-26 16:24:05 -0700

    done

--- ghi/src/components/ProductDetail.jsx
+++ ghi/src/components/ProductDetail.jsx
@@ -45,6 +45,10 @@ function ProductDetail() {
             <dt className="font-medium text-gray-900">Sold</dt>
             <dd className="mt-2 text-sm text-gray-500">{data?.sold ? "Yes" : "No"}</dd>
         </div>
+        <div className="border-t border-gray-200 pt-4">
+            <dt className="font-medium text-gray-900">Seller</dt>
+            <dd className="mt-2 text-lg text-gray-500">{data?.user_id.username}</dd>
+        </div>
         </dl>
         </div>
         <div className="grid grid-cols-1 gap-4 sm:gap-6 lg:gap-8">
